9cbd4dd9c83534655cd59394907de77242f72982
Josué W. Arévalo <124913911+HjosueA@users.noreply.github.com>
2024-09-02 17:04:49 -0600

    Update Campo many2One.py

--- Campo many2One.py
+++ Campo many2One.py
@@ -10,13 +10,13 @@ class NombreClase(models.Model):
   #Un campo many2One, muestra todos los registros que se encuentren dentro de un modelo.
 
   #Creamos un campo para mostrar los paises
-  country_id = fields.Many2one('res.country', string='País')
+  countries_id = fields.Many2one('res.country', string='País')
 
   #creamos un campo para mostrar los contactos
-  country_id = fields.Many2one('res.partner', string='Contactos')
+  partners_id = fields.Many2one('res.partner', string='Contactos')
 
   #creamos un campo para mostrar las ventas
-  country_id = fields.Many2one('sale.order', string='Ventas')
+  orders_id = fields.Many2one('sale.order', string='Ventas')
 
   #Y así sucesivamente con todos los campos Many2One que quieras crear
 
