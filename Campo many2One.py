c46d6da5ae86fb8d92cd2e40a914b545cf26a1ef
Josué W. Arévalo <124913911+HjosueA@users.noreply.github.com>
2024-09-02 16:56:40 -0600

    Update Campo many2One.py

--- Campo many2One.py
+++ Campo many2One.py
@@ -1,1 +1,24 @@
+#importar las librerias que necesites
+from odoo import models, fields, _, api
+from odoo.exceptions import UserError
+
+#Declara la clase que tendrá tu modelo y los métodos
+class NombreClase(models.Model):
+  _name='nombre.modelo'  #asi declaras el nombre de tu modelo, que será el nombre de la tabla en la base de datos
+
+  #puedes declarar todos los campos Many2One que necesites
+  #Un campo many2One, muestra todos los registros que se encuentren dentro de un modelo.
+
+  #Creamos un campo para mostrar los paises
+  country_id = fields.Many2one('res.country', string='País')
+
+  #creamos un campo para mostrar los contactos
+  country_id = fields.Many2one('res.partner', string='Contactos')
+
+  #creamos un campo para mostrar las ventas
+  country_id = fields.Many2one('sale.order', string='Ventas')
+
+  Y así sucesivamente con todos los campos Many2One que quieras crear
+
+
 
